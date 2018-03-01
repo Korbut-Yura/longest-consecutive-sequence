8fa85d3e89c23deb57ed240e92186afacb58a05c
Yura-Korbut <korbuty@mail.ru>
2018-03-02 00:43:41 +0300

    feature/solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let count = 1,
+      countMax = count;
+  if (array.length == 0) {
+    return 0;
+  }
+  else {
+    array.sort(function (a,b) {
+      return a-b;
+    })
+    
+    for (let i = 1; i < array.length; i++) {
+       if (array[i-1] == array[i]-1) {
+         count++;
+       }
+       else if (array[i-1] == array[i]) {
+        continue;   
+        }
+        else {
+            if (count > countMax) {
+           countMax = count;
+         }
+         count = 1;
+       }
+    }
+    return countMax;
+  }
 }
